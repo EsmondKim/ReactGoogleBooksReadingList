0497b478ffef8010c0b615df52b5553e7383488c
3@ <ekelectronicmail@gmail.com>
2022-01-30 12:04:13 -0600

    Delete button, changed from span to button.

--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -5,9 +5,9 @@ import "./style.css";
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
   return (
-    <span className="delete-btn, btn btn-danger" type="button" {...props}>
+    <button className="delete-btn, btn btn-danger" type="button" {...props}>
       Delete
-    </span>
+    </button>
   );
 }
 
